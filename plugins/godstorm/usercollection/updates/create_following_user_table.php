5cce82effdfccc7c2f963ae14d437c8fbbcbbce0
Khanh Nguyen <godstorm91@gmail.com>
2018-02-03 01:06:54 +0900

    update version and add table

--- plugins/godstorm/usercollection/updates/create_following_user_table.php
+++ plugins/godstorm/usercollection/updates/create_following_user_table.php
@@ -9,17 +9,17 @@ class CreateFollowingUsersTable extends Migration
 {
     public function up()
     {
-        Schema::create('godstorm_usercollection_user_collections', function(Blueprint $table) {
+        Schema::create('godstorm_usercollection_user_followings', function(Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->increments('id');
             $table->string('collection_name');
-            $table->integer('user_id');
+            $table->integer('following_user_id');
             $table->timestamps();
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('godstorm_usercollection_user_collections');
+        Schema::dropIfExists('godstorm_usercollection_user_followings');
     }
 }
